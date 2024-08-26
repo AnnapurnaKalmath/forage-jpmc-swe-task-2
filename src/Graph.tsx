1d1a6bdce896868f995013a6f1045d5403daba87
Annapurna Kalmath <kalmathannapurna@gmail.com>
2024-08-26 18:10:51 +0530

    Fix data streaming and graph rendering issues

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -33,6 +33,7 @@ class Graph extends Component<IProps, {}> {
   componentDidMount() {
     // Get element to attach the table from the DOM.
     const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    console.log('Perspective viewer element:', elem);
 
     const schema = {
       stock: 'string',
@@ -43,12 +44,23 @@ class Graph extends Component<IProps, {}> {
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
+      console.log('Perspective table created:', this.table);
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute('view','y_line');
+      elem.setAttribute('column-pivots','["stock"]');
+      elem.setAttribute('row-pivots','["timestamp"]');
+      elem.setAttribute('aggregates',`
+        {"stock" : "distinct count",
+        "top_ask_price" : "avg",
+        "top_bid_price" : "avg",
+        "timestamp" : "distinct count"
+        }`);
+        console.log('Perspective viewer attributes set');
     }
   }
 
